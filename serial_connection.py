322aa1c74f43d68e7ffa59cdf88e730ae50bfe4b
valstavros <47432144+valstavros@users.noreply.github.com>
2025-10-27 18:21:03 +0200

    Delete serial_connection.py

--- serial_connection.py
+++
@@ -1,6 +0,0 @@
-import serial
-with serial.Serial(port='com3', baudrate=9600, parity='N',stopbits=1, bytesize=8, timeout=8) as console:
-    if console.is_open:
-        print('Console successfully opened!')
-    else:
-        print('Error opening the console connection')
